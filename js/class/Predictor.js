6b15ea289be7859ae2b6351a2841bb16b6731300
Javier Moreno <84688202+ElverJavierMorenoSanchez@users.noreply.github.com>
2023-11-15 08:46:34 -0500

    add css styles and comments in the code

--- js/class/Predictor.js
+++ js/class/Predictor.js
@@ -1,10 +1,23 @@
+/**
+ * Class representing a predictor for Pico y Placa.
+ */
 class Predictor {
+  /**
+   * Create a predictor.
+   * @param {string} plateNumber - The plate number of the vehicle.
+   * @param {string} date - The date in format "YYYY-MM-DD".
+   * @param {string} time - The time in format "HH:MM".
+   */
   constructor(plateNumber, date, time) {
     this.plateNumber = plateNumber;
     this.date = date;
     this.time = time;
   }
 
+  /**
+   * Verify if the vehicle can be on the road based on Pico y Placa rules.
+   * @returns {boolean} - True if the vehicle can't be on the road, false otherwise.
+   */
   verifyPicoYPlaca() {
     const date = new Date(this.date);
     const dateDayNumber = date.getDay();
@@ -14,14 +27,20 @@ class Predictor {
     );
 
     if (this.isPicoYPlacaDay(dateDayNumber, pnLastDigit)) {
-      if (this.isPicoYPlacaHour(time)) return "No puedes circular";
+      if (this.isPicoYPlacaHour(time)) return true;
 
-      return "Puedes circular";
+      return false;
     }
 
-    return "Puedes circular";
+    return false;
   }
 
+  /**
+   * Check if the day and last digit of the plate number are in the Pico y Placa rules.
+   * @param {number} dateDayNumber - The day number of the date (0-6, where 0 is Monday).
+   * @param {number} pnLastDigit - The last digit of the plate number.
+   * @returns {boolean} - True if the day and last digit are in the Pico y Placa rules, false otherwise.
+   */
   isPicoYPlacaDay(dateDayNumber, pnLastDigit) {
     const picoYPlacaRules = {
       0: [1, 2],
@@ -34,13 +53,20 @@ class Predictor {
     return picoYPlacaRules[dateDayNumber].includes(pnLastDigit);
   }
 
+  /**
+   * Check if the time is in the Pico y Placa peak hours.
+   * @param {string} time - The time in format "HH:MM".
+   * @returns {boolean} - True if the time is in the Pico y Placa peak hours, false otherwise.
+   */
   isPicoYPlacaHour(time) {
     const hour = parseInt(time.substr(0, 2));
     const minute = parseInt(time.substr(3, 5));
 
     const isMorningPeak =
       hour >= 6 && hour < 10 && (hour !== 9 || minute <= 30);
-    const isAfternoonPeak = hour >= 16 && hour < 20;
+
+    const isAfternoonPeak =
+      hour >= 16 && hour < 20 && (hour !== 19 || minute <= 30);
 
     return isMorningPeak || isAfternoonPeak;
   }
--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 import Predictor from "./class/Predictor.js";
 
-const form = document.getElementById("form");
-const plateNumber = document.getElementById("plateNumber");
-const date = document.getElementById("date");
-const time = document.getElementById("time");
-
+/**
+ * Adds a character to an input field if the length of the current value is equal to the length of the first part and the character is not already included.
+ * Removes the character from the input field if the Backspace key is pressed and the current value ends with the character.
+ * @param {number} lengthFirstPart - The length of the first part of the input value.
+ * @param {string} character - The character to add or remove from the input value.
+ * @param {Event} event - The event object for the input field.
+ */
 function putCharacterInInput(lengthFirstPart, character, event) {
   const currentValue = event.target.value;
 
@@ -20,20 +22,93 @@ function putCharacterInInput(lengthFirstPart, character, event) {
   }
 }
 
-time.addEventListener("keydown", (e) => {
-  putCharacterInInput(2, ":", e);
-});
+/**
+ * Sets the current date and time to the given input fields.
+ * @param {HTMLInputElement} date - The input field for the date.
+ * @param {HTMLInputElement} time - The input field for the time.
+ */
+function setCurrentDateTime(date, time) {
+  const today = new Date().toLocaleDateString().slice(0, 10);
+  const currentDay = today.slice(0, 2);
+  const currentMonth = today.slice(3, 5);
+  const currentYear = today.slice(6, 10);
+  date.value = `${currentYear}-${currentDay}-${currentMonth}`;
 
-plateNumber.addEventListener("keydown", (e) => {
-  putCharacterInInput(3, "-", e);
-});
+  const currentTime = new Date();
+  const hours = String(currentTime.getHours()).padStart(2, "0");
+  const minutes = String(currentTime.getMinutes()).padStart(2, "0");
+  const formattedTime = `${hours}:${minutes}`;
+  time.value = formattedTime;
+}
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const predictor = new Predictor(plateNumber.value, date.value, time.value);
-  const result = predictor.verifyPicoYPlaca();
-
+/**
+ * Initializes the main function.
+ * @function
+ * @returns {void}
+ */
+function main() {
+  const form = document.getElementById("form");
+  const plateNumber = document.getElementById("plateNumber");
+  const date = document.getElementById("date");
+  const time = document.getElementById("time");
+  const cleanBtn = document.getElementById("cleanBtn");
   const resultContainer = document.getElementById("result");
-  resultContainer.innerHTML = result;
-});
+
+  /**
+   * Clears the plate number input and result container, and sets the current date and time.
+   * @function
+   * @returns {void}
+   */
+  cleanBtn.addEventListener("click", () => {
+    plateNumber.value = "";
+    resultContainer.style.display = "none";
+    setCurrentDateTime(date, time);
+  });
+
+  /**
+   * Adds a hyphen to the plate number input after the third character.
+   * @function
+   * @param {number} index - The index where the hyphen should be added.
+   * @param {string} character - The character to be added (in this case, "-").
+   * @param {event} e - The keydown event.
+   * @returns {void}
+   */
+  plateNumber.addEventListener("keydown", (e) => {
+    putCharacterInInput(3, "-", e);
+  });
+
+  /**
+   * Verifies if the plate number is valid and if the car is allowed to be on the road at the given date and time.
+   * @function
+   * @param {event} e - The submit event.
+   * @returns {void}
+   */
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const plateNumberDigit = plateNumber.value.substr(
+      3,
+      plateNumber.value.length - 1
+    );
+
+    if (isNaN(plateNumberDigit)) {
+      alert("Please introduce a valid plate number");
+      return;
+    }
+
+    const predictor = new Predictor(plateNumber.value, date.value, time.value);
+    const isPicoYPlaca = predictor.verifyPicoYPlaca();
+
+    if (isPicoYPlaca) resultContainer.classList.add("pico_placa");
+    else resultContainer.classList.remove("pico_placa");
+
+    resultContainer.style.display = "block";
+    resultContainer.innerHTML = isPicoYPlaca
+      ? "Car can't be on the road"
+      : "Car can be on the road";
+  });
+
+  setCurrentDateTime(date, time);
+}
+
+main();
